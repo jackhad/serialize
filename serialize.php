cb184b4a38481dff9bc1c926df0a2c20b5c94dab
Aaronkeith <jackhad@gmail.com>
2014-02-19 12:30:11 +0530

    Create serialize.php

---
+++ serialize.php
@@ -0,0 +1,16 @@
+<select id="Realtionship" class="validate[required]" name="Realtionship">
+<option value="">Choose Option</option>
+<option value="112">Husband</option>
+<option value="113">Wife</option>
+</select> 
+<input type="text" name="Relname" id="Relname">
+<input type="submit" name="submit" >
+
+
+<?php 
+$relation=array();
+$reltype=$_POST['Realtionship'];
+$relation[$reltype]=$_POST['Relname']; 
+$reladata=serialize($relation);
+print_r($reladata) // you can insert this valiable into table field
+?>
